3f5eec32b075c6d0acd968d105858b94e14bea99
Ronak <ronakpatel5177@gmail.com>
2024-02-05 22:23:40 +0530

    discord app version 1.0.0 completed

---
+++ PROJECT-03-Discord-Bot/command.js
@@ -0,0 +1,25 @@
+const { REST, Routes } = require('discord.js');
+
+const TOKEN = ""
+const CLIENT_ID = "";
+const commands = [
+  {
+    name: 'create',
+    description: 'Creates a new shorturl!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+
+(async ()=>{
+    try {
+        console.log('Started refreshing application (/) commands.');
+      
+        await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+      
+        console.log('Successfully reloaded application (/) commands.');
+      } catch (error) {
+        console.error(error);
+      }
+})();
